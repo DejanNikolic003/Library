96e63f1c40c86c70f569d9862079f79e874446cd
DejanNikolic003 <knolekjhnny@gmail.com>
2025-04-23 11:28:09 +0200

    Added function to list the books, and handling the event listeners.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const myLibrary = [];
 
 class Book {
   constructor(title, author, pages) {
-    this.id = 1;
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -24,3 +24,42 @@ const removeBookFromLibrary = (bookId) => {
 const getBookIndexById = (bookId) => {
   return myLibrary.findIndex((book) => book.id === bookId);
 };
+
+const listBooks = () => {
+  const bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const bookWrapper = document.createElement("div");
+    const bookTitle = document.createElement("h1");
+    const bookRemoveButton = document.createElement("button");
+
+    bookWrapper.classList.add("book");
+    bookWrapper.dataset.id = book.id;
+    bookTitle.textContent = book.title;
+    bookRemoveButton.classList.add("removeBtn");
+    bookRemoveButton.textContent = "Remove";
+
+    bookWrapper.append(bookTitle, bookRemoveButton);
+    bookContainer.append(bookWrapper);
+  });
+
+  handleEventListeners();
+};
+
+const handleEventListeners = () => {
+  const removeButtons = document.querySelectorAll(".removeBtn");
+
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const book = event.target.closest(".book");
+
+      removeBookFromLibrary(book.dataset.id);
+      listBooks();
+    });
+  });
+};
+
+(function () {
+  listBooks();
+})();
